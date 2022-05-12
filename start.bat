1163338e2a8b1e1e649e6963d609f7feeebdbf40
iamdoubz <4871781+iamdoubz@users.noreply.github.com>
2022-05-12 11:12:13 -0500

    Add windows startup scripts

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0start.ps1"
+exit
---
+++ start.ps1
@@ -0,0 +1,1 @@
+Start-Process -WindowStyle hidden -FilePath ./go-shadowsocks2.exe -ArgumentList "-c `"ss://AEAD_CHACHA20_POLY1305:-mysupersecretpassword-@socks5.my.domain:8443`" -verbose -socks :1080 -u -udptun :8053=8.8.8.8:53,:8054=8.8.4.4:53 -tcptun :8053=8.8.8.8:53,:8054=8.8.4.4:53"
